6a149a66f1437d964fb294d113d7944c16770502
Thet Minn Htun <59432845+thetminnhtun-scm@users.noreply.github.com>
2023-03-15 13:40:19 +0630

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,57 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.jsx',
+    ],
+
+    theme: {
+        container: {
+            center: true,
+        },
+        screens: {
+            sm: '480px',
+            md: '768px',
+            lg: '976px',
+            xl: '1440px',
+        },
+        extend: {
+            fontFamily: {
+                comfortaa: "'Comfortaa', cursive",
+                josefin: "'Josefin Sans', sans-serif",
+            },
+            colors: {
+                primary: {
+                    50: '#fff7ed',
+                    100: '#ffedd5',
+                    200: '#fed7aa',
+                    300: '#fcd34d',
+                    400: '#fb923c',
+                    500: '#f97316',
+                    600: '#ea580c',
+                    700: '#c2410c',
+                    800: '#9a3412',
+                    900: '#7c2d12',
+                },
+                secondary: {
+                    50: '#fef2f2',
+                    100: '#fee2e2',
+                    200: '#fecaca',
+                    300: '#fca5a5',
+                    400: '#f87171',
+                    500: '#ef4444',
+                    600: '#dc2626',
+                    700: '#b91c1c',
+                    800: '#991b1b',
+                    900: '#7f1d1d',
+                },
+            },
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms')],
+};
